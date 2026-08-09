b222f09ac6f119e5e997fd876b685ecc3715202b
Laurent Koné <lkone@Laurents-MacBook-Pro.local>
2026-08-09 02:35:28 +0200

    feat: add nvim markdown lua custom config

---
+++ lazyvim/plugins/markdown.lua
@@ -0,0 +1,33 @@
+return {
+  -- First, ensure the LazyVim markdown "extra" is loaded
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+
+  -- Second, override the render-markdown settings
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      -- Customize code block behavior here
+      code = {
+        -- Turn code block rendering on or off completely
+        enabled = false,
+        
+        -- Style of the code block. Options: 'normal', 'language', 'full'
+        -- 'full' adds a background color padding across the screen
+        style = "full",
+        
+        -- Where to put the language name. Options: 'right', 'left'
+        position = "right",
+        
+        -- Custom width of the code block background
+        width = "block",
+        
+        -- Padding around the block
+        left_pad = 2,
+        right_pad = 2,
+        
+        -- The highlight group for the code block background
+        highlight = "RenderMarkdownCode",
+      },
+    },
+  },
+}
